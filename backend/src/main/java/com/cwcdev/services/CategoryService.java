ea1e8bee498f6bdbd21964c522f480d83fa535b1
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-27 13:59:50 -0300

    buscando pelo id

--- backend/src/main/java/com/cwcdev/services/CategoryService.java
+++ backend/src/main/java/com/cwcdev/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.cwcdev.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,12 @@ public class CategoryService {
 		List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	    return listDTO;
 	
+	}
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj =   repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 
 }
