6723ee6a9475cec3cb99aec7770c37419545caca
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-27 11:51:55 -0300

    category resources

---
+++ backend/src/main/java/com/cwcdev/resources/CategoryResource.java
@@ -0,0 +1,28 @@
+package com.cwcdev.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cwcdev.entities.Category;
+
+/*Complementa Controlador REST*/
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoryResource {
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = new ArrayList<Category>();
+		list.add(new Category(1L,"Livros"));
+		list.add(new Category(2L,"Periféricos"));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
