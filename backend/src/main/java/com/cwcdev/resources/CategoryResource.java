8982bb3ada09432a80239a1684656ab35efdcadc
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-27 12:15:09 -0300

    criando service/repository e import.sql

--- backend/src/main/java/com/cwcdev/resources/CategoryResource.java
+++ backend/src/main/java/com/cwcdev/resources/CategoryResource.java
@@ -1,14 +1,15 @@
 package com.cwcdev.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cwcdev.entities.Category;
+import com.cwcdev.services.CategoryService;
 
 /*Complementa Controlador REST*/
 
@@ -16,12 +17,16 @@ import com.cwcdev.entities.Category;
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	
+	
 	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<Category>();
-		list.add(new Category(1L,"Livros"));
-		list.add(new Category(2L,"Periféricos"));
+		
+		List<Category>list = service.findAll();
+		
 		return ResponseEntity.ok().body(list);
 	}
 
---
+++ backend/src/main/java/com/cwcdev/services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.cwcdev.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cwcdev.entities.Category;
+import com.cwcdev.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category>findAll(){
+		return repository.findAll();
+	
+	
+	}
+
+}
