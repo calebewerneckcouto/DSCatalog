276a8fb82abe221fb6f8fa287f7a34e61d701592
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-27 11:31:09 -0300

    criando projeto dscatalog bakend

---
+++ backend/src/main/java/com/cwcdev/CwcdevApplication.java
@@ -0,0 +1,13 @@
+package com.cwcdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwcdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwcdevApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cwcdev/CwcdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwcdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwcdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
