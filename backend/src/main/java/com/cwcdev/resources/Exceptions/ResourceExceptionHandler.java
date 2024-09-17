15dec6e158ed1a0a04b47afb9ee96ea9b5f859dc
calebewerneckcouto <calebewerneck@hotmail.com>
2024-09-16 21:23:05 -0300

    Custom Validation Response

--- backend/src/main/java/com/cwcdev/resources/Exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/cwcdev/resources/Exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,30 +17,46 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
-	 	
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
-	 	err.setTimestamp(Instant.now());
-	 	err.setStatus(status.value());
-	 	err.setError("Resource not found");
-	 	err.setMessage(e.getMessage());
-	 	err.setPath(request.getRequestURI());
-	 	return ResponseEntity.status(status).body(err);
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
 	}
-	
-	
+
 	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> entityNotFound(DatabaseException e, HttpServletRequest request){
-	 	HttpStatus status = HttpStatus.BAD_REQUEST;
-		
+	public ResponseEntity<StandardError> entityNotFound(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+
 		StandardError err = new StandardError();
-	 	err.setTimestamp(Instant.now());
-	 	err.setStatus(status.value());
-	 	err.setError("Database Exception");
-	 	err.setMessage(e.getMessage());
-	 	err.setPath(request.getRequestURI());
-	 	return ResponseEntity.status(status).body(err);
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> Validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;// codigo 422 ( alguma entidade nao foi processada)
+
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+                  err.addErrors(f.getField(),f.getDefaultMessage());
+		}
+		return ResponseEntity.status(status).body(err);
 	}
 
 }
