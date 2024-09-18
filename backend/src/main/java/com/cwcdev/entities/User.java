359160d80281f8893e1e35c7045111fff49866e8
calebewerneckcouto <calebewerneck@hotmail.com>
2024-09-18 17:56:42 -0300

    Implantando Segurança no projeto

--- backend/src/main/java/com/cwcdev/entities/User.java
+++ backend/src/main/java/com/cwcdev/entities/User.java
@@ -93,6 +93,21 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+
+		}
+
+		return false;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
