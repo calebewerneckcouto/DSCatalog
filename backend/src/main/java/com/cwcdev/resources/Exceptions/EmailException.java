b1aa9e4c0058e5db39d265bd9f8fbd930b5709b0
calebewerneckcouto <calebewerneck@hotmail.com>
2024-11-07 13:59:02 -0300

    Comecando primeiro passo da recuperação de senha

---
+++ backend/src/main/java/com/cwcdev/resources/Exceptions/EmailException.java
@@ -0,0 +1,9 @@
+package com.cwcdev.resources.Exceptions;
+
+@SuppressWarnings("serial")
+public class EmailException extends RuntimeException {
+
+	public EmailException(String msg) {
+		super(msg);
+	}
+}
---
+++ backend/src/main/java/com/cwcdev/services/AuthService.java
@@ -0,0 +1,13 @@
+package com.cwcdev.services;
+
+import org.springframework.stereotype.Service;
+
+import com.cwcdev.dto.EmailDTO;
+@Service
+public class AuthService {
+
+	public void createRecoverToken(EmailDTO body) {
+		
+	}
+
+}
