5333d7ad1a1e88b5d73479a0b63972ba2305748e
calebewerneckcouto <calebewerneck@hotmail.com>
2024-11-09 15:58:43 -0300

    Tratamento da excecao Email Exception

--- backend/src/main/java/com/cwcdev/resources/Exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/cwcdev/resources/Exceptions/ResourceExceptionHandler.java
@@ -58,5 +58,19 @@ public class ResourceExceptionHandler {
 		}
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	
+	@ExceptionHandler(EmailException.class)
+	public ResponseEntity<StandardError> email(EmailException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Email Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 
 }
